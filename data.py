4bdfc5c0f7856feafc1634975b8a5f2e0fc8ef87
ad1012 <69478216+ad1012@users.noreply.github.com>
2022-03-02 01:00:07 -0600

    Update data.py

--- data.py
+++ data.py
@@ -5,26 +5,28 @@ import numpy as np
 def plotFigure(data_path):
     with open(data_path) as csv_file:
         df = csv.DictReader(csv_file)
-        fig = px.scatter(df,x="Temperature", y="Ice-cream Sales( â‚¹ )")
+        fig = px.scatter(df,x="Coffee In ml", y="Hours of Sleep")
         fig.show()
 
 def getDataSource(data_path):
-    ice_cream_sales = []
     cups_of_coffee = []
+    sleep_in_hours = []
+    
     with open(data_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            ice_cream_sales.append(float(row["Temperature"]))
-            cups_of_coffee.append(float(row["Ice-cream Sales( â‚¹ )"]))
+            cups_of_coffee.append(float(row["Coffee in ml"]))
+            sleep_in_hours.append(float(row["sleep in hours"]))
+            
 
-    return {"x" : ice_cream_sales, "y": cups_of_coffee}
+    return {"x" : cups_of_coffee, "y": sleep_in_hours}
 
 def findCorrelation(datasource):
     correlation = np.corrcoef(datasource["x"], datasource["y"])
-    print("Correlation between Temperature vs Ice Cream Sales :-  \n--->",correlation[0,1])
+    print("Correlation between Cups of Coffee vs Hours of Sleep :-  \n--->",correlation[0,1])
 
 def setup():
-    data_path  = "./data/Ice-Cream vs Cold-Drink vs Temperature - Ice Cream Sale vs Temperature data.csv"
+    data_path  = "./data/cups of coffee vs hours of sleep.csv"
 
     #datasource = getDataSource(data_path)
     #findCorrelation(datasource)
