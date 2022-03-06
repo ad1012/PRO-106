626d2b5594dd76a6c8858ee78760e8eaf993162e
ad1012 <69478216+ad1012@users.noreply.github.com>
2022-03-06 12:19:33 -0600

    Update data.py

--- data.py
+++ data.py
@@ -5,28 +5,26 @@ import numpy as np
 def plotFigure(data_path):
     with open(data_path) as csv_file:
         df = csv.DictReader(csv_file)
-        fig = px.scatter(df,x='Coffee In ml', y='sleep in hours')
+        fig = px.scatter(df,x="Days Present", y="Marks In Percentage")
         fig.show()
 
 def getDataSource(data_path):
-    cups_of_coffee = []
-    sleep_in_hours = []
-    
+    marks_in_percentage = []
+    days_present = []
     with open(data_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            sleep_in_hours.append(float(row["sleep in hours"]))
-            cups_of_coffee.append(float(row["Coffee in ml"]))
-            
-            
-    return {"x" : sleep_in_hours, "y" : cups_of_coffee }
+            marks_in_percentage.append(float(row["Marks In Percentage"]))
+            days_present.append(float(row["Days Present"]))
+
+    return {"x" : marks_in_percentage, "y": days_present}
 
 def findCorrelation(datasource):
     correlation = np.corrcoef(datasource["x"], datasource["y"])
-    print("Correlation between Cups of Coffee vs Hours of Sleep :-  \n--->",correlation[0,1])
+    print("Correlation between Marks in percentage and Days present :-  \n--->",correlation[0,1])
 
 def setup():
-    data_path  = "./data/cups of coffee vs hours of sleep.csv"
+    data_path  = "./data/Student Marks vs Days Present.csv"
 
     datasource = getDataSource(data_path)
     findCorrelation(datasource)
